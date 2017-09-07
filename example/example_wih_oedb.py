8909c791563b482285a38ad8d8cd81d8c6319684
Chloé Lucas <RL-INSTITUT\chloe.lucas@solydxk>
2017-09-07 14:05:59 +0200

    improvement of examples

--- example/example_wih_oedb.py
+++ example/example_wih_oedb.py
@@ -10,6 +10,7 @@ import logging
 import os
 import pandas as pd
 from oemof import db
+import numpy as np
 
 
 try:
@@ -22,117 +23,70 @@ from git_repos.hydropowerlib.hydropowerlib import hydropower_plant as hpp
 
 logging.getLogger().setLevel(logging.INFO)
 
-if __name__ == '__main__':
-    conn_oedb = db.connection(section='oedb')
 
+conn_oedb = db.connection(section='oedb')
+
+try:
+    # Get power plants in Thuringe with their id, electrical capacity, location, start-up date, and the id of the raster cell containing the modeled runoff
     sql_read = """
-    WITH plants AS (SELECT reg.id,reg.start_up_date, reg.electrical_capacity, reg_dp.dp_geom, reg_dp.nam 
-    FROM supply.ego_renewable_power_plants_germany_hydro_mview reg, supply.fred_dp_res_powerplant_hydro_on_river_mview reg_dp
-    WHERE reg.id=reg_dp.id) 
-
-    WITH p_w AS (SELECT DISTINCT ON (plants.id) plants.id, plants.start_up_date, plants.electrical_capacity, plants.dp_geom, plants.nam, watergap.gid
-    FROM plants, hydrolib.watergap_geoms watergap
-    WHERE st_dwithin(st_transform(plants.dp_geom,3035), st_transform(watergap.geom,3035), 10000)
-    ORDER BY plants.id, st_distance(st_transform(plants.dp_geom,3035), st_transform(watergap.geom,3035)))        
+    WITH reg_TH AS (SELECT reg_on_wg.id, reg_on_wg.dp_geom, reg_on_wg.wg_id
+    FROM supply.fred_dp_res_powerplant_hydro_on_wg_mview reg_on_wg, orig_vg250.vg250_2_lan bl
+    WHERE st_contains(st_TRANSFORM(bl.geom,3035),ST_TRANSFORM(reg_on_wg.dp_geom,3035)) AND bl.gen='Thüringen')
+    SELECT reg_TH.id,reg_TH.dp_geom,reg_TH.wg_id,reg.start_up_date, reg.electrical_capacity
+    FROM reg_TH, supply.ego_renewable_power_plants_germany_hydro_mview reg
+    WHERE reg_TH.id=reg.id
     """
-    #TO BE CONTINUED
-    df = pd.read_sql(sql_read, con)
+    plant_reg = pd.read_sql(sql_read, conn_oedb)
 
-    try:
-        read_and_plot(conn_oedb)
-    finally:
-        conn_oedb.close()
-    print(time.time)
-    logger.info('...database connections closed...')
-    logger.info('...script successfully executed in {:.2f} seconds. Goodbye!'
-        .format(time.time() - start_time))
+    # Feel free to chage this to try another year
+    year_to_simulate=2009
 
+    # Counts the plants simulated = the plants for which runoff data from the year to simulate was available
+    plants_with_ts=0
 
-def read_river_data(filename, datetime_column='Unnamed: 0',
-                      **kwargs):
-    r"""
-    Fetches weather data from a file.
+    # Sums the energy of all plants
+    energy=0
 
-    The files are located in the example folder of the windpowerlib.
+    # DataFrame of all plants
+    plants_df=pd.DataFrame(columns=['P_n','dV_n','h_n','dV_rest','turb_type','simu','power_output'])
+    plants_df=plants_df.astype(dtype={'P_n':float,'dV_n':float,'h_n':float,'dV_rest':float,'turb_type':str,'simu':bool,'power_output':pd.Series})
 
-    Parameters
-    ----------
-    filename : string
-        Filename of the weather data file.
-    datetime_column : string
-        Name of the datetime column of the weather DataFrame.
+    for i in plant_reg.index:
+        # Read the runoff time series of the raster cell in which the plant is (one time series per year)
+        sql_read= """SELECT year, time_series FROM hydrolib.watergap_runoff
+        WHERE geom_id=%s """ %plant_reg.loc[i,'wg_id']
+        discharge=pd.read_sql(sql_read, conn_oedb)
 
-    Other Parameters
-    ----------------
-    datapath : string, optional
-        Path where the weather data file is stored.
-        Default: 'windpowerlib/example'.
+        simu=False
 
-    Returns
-    -------
-    pandas.DataFrame
-        Contains weather data time series.
+        # Remove empty 2/29 cell on leap years (no leap years in watergap) and concatenate in one time series over several years
+        ts_df=pd.DataFrame(columns=['dV'])
+        for j in discharge.index:
+            index_temp1=pd.date_range('1/1/%s'%discharge.loc[j,'year'], periods=59, freq='D')
+            index_temp2=pd.date_range('3/1/%s'%discharge.loc[j,'year'], periods=306, freq='D')
+            index_temp=index_temp1.append(index_temp2)
+            discharge_temp=np.asarray(discharge.loc[j, 'time_series'])
+            discharge_temp=discharge_temp[~np.isnan(discharge_temp)]
+            ts_df=ts_df.append(pd.DataFrame(index=index_temp,data={'dV':discharge_temp}))
+            # If the raster cell has modeled runoff data for the year to simulate, simulation of this plant is possible
+            if discharge.loc[j,'year']==year_to_simulate:
+                plants_with_ts=plants_with_ts+1
+                power_outputs=pd.DataFrame(index=index_temp)
+                simu=True
+        if simu==True:
+            # Define the HydropowerPlant object and the ModelChain object based on available data
+            my_hpp=hpp.HydropowerPlant(P_n=plant_reg.loc[i,'electrical_capacity']*1000,id=plant_reg.loc[i,'id'])
+            my_mc=modelchain.Modelchain(HPP=my_hpp,dV=ts_df.loc[ts_df.index.year==year_to_simulate,:],dV_hist=ts_df)
+            my_mc.run_model()
+            power_outputs[plant_reg.loc[i,'id']]=my_hpp.power_output
+            energy=energy+(my_hpp.power_output.sum()*24)
+            plants_df.loc[my_hpp.id] = {'P_n': my_hpp.P_n, 'dV_n': my_hpp.dV_n, 'h_n': my_hpp.h_n, 'dV_rest': my_hpp.dV_rest,
+                                     'turb_type': my_hpp.turb_type, 'simu': simu,
+                                     'power_output': my_hpp.power_output}
+        else:
+            plants_df.loc[plant_reg.loc[i,'id']] = {'P_n': plant_reg.loc[i,'electrical_capacity']*1000, 'dV_n': None, 'h_n': None,
+                                        'dV_rest': None, 'turb_type': None, 'simu': simu, 'power_output': None}
 
-    """
-    if 'datapath' not in kwargs:
-        kwargs['datapath'] = os.path.join(os.path.split(
-            os.path.dirname(__file__))[0], 'example')
-
-    file = os.path.join(kwargs['datapath'], filename)
-    df = pd.read_csv(file)
-    return df.set_index(pd.to_datetime(df[datetime_column])).tz_localize(
-        'UTC').tz_convert('Europe/Berlin').drop(datetime_column, 1)
-
-# Read weather data from csv
-river_data = read_river_data('river_raon.csv')
-
-# Specifications of the hydropower plant
-hydro_Raon = {
-    'H_n': 4.23,
-    'Q_n': 12,
-    'W_n': 0.8,
-    'P_n':400000,
-    'turb_num':1,
-    'eta_gen':0.95}
-
-
-# Initialize HydropowerPlant objects
-hpp_ex = hpp.HydropowerPlant(**hydro_Raon)
-print(hpp_ex.turbine_type)
-print(hpp_ex.Q_n)
-
-
-# Specifications of the modelchain data
-modelchain_data = {
-    'g_model': 'default',
-    'rho_model': 'default'}
-
-# Calculate turbine power output
-mc_hpp_ex = modelchain.Modelchain(hpp_ex, **modelchain_data).run_model(
-    river_data)
-hpp_ex.power_output = mc_hpp_ex.power_output
-
-# Plot turbine power output
-if plt:
-    hpp_ex.power_output.plot(legend=True, label='Example')
-    plt.show()
-else:
-    print(hpp_ex.power_output)
-
-# Plot power (coefficient) curves
-if plt:
-    if hpp_ex.eta_turb_values is not None:
-        hpp_ex.eta_turb_values.plot(style='*', title='Example')
-        plt.show()
-else:
-    if hpp_ex.eta_turb_values is not None:
-        print("The efficency value with a normalized waterflow of 0,6: {0}".format(
-            hpp_ex.eta_turb_values.eta_turb[0.6]))
-
-print(hpp_ex.power_output)
-output_april=hpp_ex.power_output.loc['2017-04-12 00:00:00':'2017-04-30 23:00:00']
-print(output_april.sum())
-output_may=hpp_ex.power_output.loc['2017-05-01 00:00:00':'2017-05-31 23:00:00']
-print(output_may.sum())
-
-logging.info('Done!')
+    logging.info('\t%d from %d plants simulated \n\t\t\tEnergy produced : %d GWh' %(plants_with_ts,len(plants_df.index),energy/1000000000))
+finally:
+    conn_oedb.close()
--- example/example_with_csv.py
+++ example/example_with_csv.py
@@ -1,5 +1,5 @@
 """
-The ``basic_example`` module shows a simple usage of the hydropowerlib.
+The ``example_with_csv`` module shows a simple usage of the hydropowerlib.
 
 """
 
@@ -13,7 +13,11 @@ import glob
 
 
 try:
+    import matplotlib
     from matplotlib import pyplot as plt
+    from matplotlib import dates as mdates
+    matplotlib.style.use('seaborn-deep')
+    matplotlib.rcParams.update({'font.size': 12})
 except ImportError:
     plt = None
 
@@ -33,28 +37,35 @@ for file in filenames:
     df = pd.read_csv(file, index_col='date', parse_dates=True, infer_datetime_format=True)
     dV_hist_Raon = dV_hist_Raon.append(df)
 
-print(dV_hist_Raon)
 # Definition of the hydropower plant
 hydro_Raon = hpp.HydropowerPlant(id='HydroRaon',dV_n= 12,h_n=4.23,P_n=400000,turb_num=1,turb_type='Kaplan')
 
 # Definition of the modelchain
 mc_hpp_raon = modelchain.Modelchain(HPP=hydro_Raon,dV=dV_Raon,dV_hist=dV_hist_Raon)
 
+
 # Run model
 mc_hpp_raon.run_model()
-hydro_Raon.power_output
+
+# Get outputs of each month
+output_april=hydro_Raon.power_output.loc['2017-04-01 00:00:00':'2017-04-30 23:00:00']
+output_may=hydro_Raon.power_output.loc['2017-05-01 00:00:00':'2017-05-31 23:00:00']
+
+logging.info('\tEnergy production in April : %d MWh\n\t\t\tEnergy production in May : %d MWh'%(output_april.sum()/1000000,output_may.sum()/1000000))
 
 # Plot turbine power output
 if plt:
-    hydro_Raon.power_output.plot(legend=True, label='Example')
-    dV_Raon.plot(legend=True, label='dV')
+    fig,ax=plt.subplots(1)
+    plt.plot(hydro_Raon.power_output.index, hydro_Raon.power_output.values/1000)
+    plt.grid(b=True, which='major',axis='y')
+    plt.ylabel("Power output [kW]")
+    plt.xlabel("Day")
+    plt.xticks(rotation=45)
+    plt.xlim('2017-04-01 00:00:00','2017-05-31 23:00:00')
+    ax.xaxis.set_major_locator(mdates.DayLocator(interval=5))  # to get a tick every 15 minutes
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%m-%d'))  # optional formatting
     plt.show()
 else:
     print(hydro_Raon.power_output)
 
-output_april=hydro_Raon.power_output.loc['2017-04-12 00:00:00':'2017-04-30 23:00:00']
-print(output_april.sum())
-output_may=hydro_Raon.power_output.loc['2017-05-01 00:00:00':'2017-05-31 23:00:00']
-print(output_may.sum())
-print(hydro_Raon.P_n)
 logging.info('Done!')
